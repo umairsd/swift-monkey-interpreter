7a060eadbf510f3ea3d1c73edfef8a0b2f222fe8
Umair Saeed <umair.saeed@gmail.com>
2023-10-24 14:48:13 -0700

    Rename the executable's name to monkey.

--- MonkeyInterpreter/Package.swift
+++ MonkeyInterpreter/Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
   name: "MonkeyInterpreter",
+  products: [
+    .executable(name: "monkey", targets: ["MonkeyInterpreter"])
+  ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
