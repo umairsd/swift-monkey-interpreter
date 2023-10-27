324cabf967079c817c654f5d99881eb98b466f49
Umair Saeed <umair.saeed@gmail.com>
2023-10-27 11:36:36 -0700

    [AST] Add a type to represent an Expression Statement.

---
+++ MonkeyInterpreter/Sources/AST/ExpressionStatement.swift
@@ -0,0 +1,31 @@
+// Created on 10/27/23.
+
+import Foundation
+import Token
+
+/// An expression statement is a statement that consists solely of one expression.
+/// It is only a wrapper. Consider the example:
+/// 
+/// ```
+/// let x = 5;
+/// x + 10;
+/// ```
+///
+/// The second line above is an "expression statement".
+///
+public class ExpressionStatement: Statement {
+
+  /// The expression that's being wrapped.
+  public let expression: Expression
+
+
+  public init(token: Token, expression: Expression) {
+    self.expression = expression
+    self.token = token
+  }
+
+
+  // MARK: - Protocol (Statement)
+
+  public let token: Token
+}
